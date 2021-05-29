128eac8b0431d908bc04e20e35a8b8a72c9daf70
Jakub Jazdzewski <jakubjazdzewski@wp.pl>
2021-05-29 18:19:36 +0200

    rozwiązanie 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,22 +2,89 @@ package com.company;
 
 class Animal
 {
+    final String species;
+    private Double weight;
 
+    Animal(String species) {
+        this.species = species;
+        this.weight = weight;
+        if(species == "Pies")
+        {
+            weight = 10.0;
+        }else if(species == "Kot")
+        {
+            weight = 5.0;
+        }
+    }
+
+    void feed()
+    {
+        if(weight>0) {
+            weight++;
+            System.out.println("Waga Twojego zwierzaka "+species+" : " + weight);
+        }
+        else
+        {
+            System.out.println("Zabiles go!");
+        }
+
+    }
+
+    void takeForAWalk()
+    {
+        weight--;
+        if(weight<4 && weight>0) {
+            System.out.println("Twoj zwierzak: "+species+"jest glodny, nakarm go!");
+            System.out.println("Waga Twojego zwierzaka: "+species+" : " + weight);
+        }
+        else if(weight == 0) {
+            System.out.println("Twoj: "+species+" umarl");
+        }
+        else if(weight <0)
+            System.out.println("Zabiles go!");
+        else
+        {
+            System.out.println("Waga Twojego zwierzaka: "+species+" : " + weight);
+        }
+    }
 }
 
 class Human
 {
+    Human (Animal pet)
+    {
+
+    }
 
 }
 
 class Phone
 {
-    
+
 }
 
 public class Main {
 
     public static void main(String[] args) {
+        Animal zwierze = new Animal("Pies");
+        Animal zwierze2 = new Animal("Kot");
+        Human human = new Human(zwierze);
+        zwierze.feed();
+        for(int i=0;i<10;i++)
+            zwierze.takeForAWalk();
+        zwierze.takeForAWalk();
+        zwierze.takeForAWalk();
+        zwierze.feed();
 
+        zwierze2.takeForAWalk();
+        zwierze2.takeForAWalk();
+        zwierze2.takeForAWalk();
+        zwierze2.takeForAWalk();
+        zwierze2.takeForAWalk();
+        zwierze2.feed();
+        zwierze2.feed();
+        zwierze2.takeForAWalk();
+        zwierze2.takeForAWalk();
+        zwierze2.feed();
     }
 }
